2c3ad438bdab9308eab7fd34ffd51a9dff1fd175
Vlad Mazur <vladmazur93@gmail.com>
2016-04-29 16:05:46 +0300

    Continue adding paging 1) Add implementation for Product ListView model 2) Add page num buttons to the product view 3) Change how ULRs looks in RouteConfig 4) more tests

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -20,10 +21,21 @@ namespace SportsStore.WebUI.Controllers
         // GET: Product
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
                 .OrderBy(p => p.ProductID)
-                .Skip((page-1) * pageSize)
-                .Take(pageSize));
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Products.Count()
+                }
+                
+            };
+            return View(model);
         }
     }
 }
