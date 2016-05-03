a853b2f7a5c86942f09ad4e247bcbfda36b4ea9f
Vlad Mazur <vladmazur93@gmail.com>
2016-05-03 12:25:29 +0300

    Add to cart button

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -122,8 +122,84 @@ namespace SportsStore.UnitTests
             // Assert
             Assert.AreEqual(result.Length, 2);
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
-            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
-       
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");      
+        }
+
+        [TestMethod]
+        public void Can_Create_Category()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product {ProductID = 2, Name = "P2", Category = "Apples" },
+                new Product {ProductID = 3, Name = "P3", Category = "Plums" },
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges" }
+            });
+
+            NavController controller = new NavController(mock.Object);
+
+            //  Action
+            string[] results = ((IEnumerable<string>)controller.Menu().Model).ToArray();
+
+            //  Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product {ProductID = 4, Name = "P2", Category = "Oranges" }
+            });
+
+            NavController controller = new NavController(mock.Object);
+
+            string categoryToSelect = "Apples";
+
+            //  Action
+            string result = controller.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            //  Assert
+            Assert.AreEqual(categoryToSelect, result);
+        }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3" }
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.pageSize = 3;
+
+            //  Action
+            int res1 = ((ProductsListViewModel)controller.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductsListViewModel)controller.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductsListViewModel)controller.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductsListViewModel)controller.List(null).Model).PagingInfo.TotalItems;
+
+            //  Assert
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
         }
     }
 }
