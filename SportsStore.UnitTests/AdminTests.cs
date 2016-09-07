80468c5e1ecd0055231051ce37b697f1eabf14f4
Vlad Mazur <vladmazur93@gmail.com>
2016-09-07 19:00:29 +0300

    Just save last changes

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using Moq;
+
+namespace SportsStore.UnitTests
+{
+    /// <summary>
+    /// Сводное описание для AdminTests
+    /// </summary>
+    [TestClass]
+    public class AdminTests
+    {
+        public AdminTests()
+        {
+            //
+            // TODO: добавьте здесь логику конструктора
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" }
+            });
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual(result[0].Name, "P1");
+            Assert.AreEqual(result[1].Name, "P2");
+            Assert.AreEqual(result[2].Name, "P3");
+        }
+    }
+}
